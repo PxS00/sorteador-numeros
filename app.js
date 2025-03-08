f92234c1c3c387152a80cbe7b5fed07aefb00a3c
Rossoni <lucas.rossonidd@gmail.com>
2025-03-08 17:09:26 -0300

    Alteração nas variaveis para obter somente números

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 function sortear(){
-    let quantidadeDeNumeros = document.getElementById('quantidade').ariaValueMax;
-    let numeroMinimo = document.getElementById('de').value;
-    let numeroMaximo = document.getElementById('ate').value;
+    let quantidadeDeNumeros = Number(document.getElementById('quantidade').value);
+    let numeroMinimo = Number(document.getElementById('de').value);
+    let numeroMaximo = Number(document.getElementById('ate').value);
 
     alert(`Quantidade: ${quantidadeDeNumeros}`);
     alert(`Do número: ${numeroMinimo}`);
-    alert(`Quantidade: ${numeroMaximo}`);
+    alert(`Até o número: ${numeroMaximo}`);
 }
