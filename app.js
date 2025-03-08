4c3ff1f0985636cf61eda70b0e1359cf4223296b
Rossoni <lucas.rossonidd@gmail.com>
2025-03-08 17:00:06 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function sortear(){
+    let quantidadeDeNumeros = document.getElementById('quantidade').ariaValueMax;
+    let numeroMinimo = document.getElementById('de').value;
+    let numeroMaximo = document.getElementById('ate').value;
+
+    alert(`Quantidade: ${quantidadeDeNumeros}`);
+    alert(`Do número: ${numeroMinimo}`);
+    alert(`Quantidade: ${numeroMaximo}`);
+}
