fe195ff2d0e3ec053dd3f572f8ea6ccf1819898a
Rossoni <lucas.rossonidd@gmail.com>
2025-03-08 18:36:45 -0300

    Criando fnção para alterar status do botão

--- app.js
+++ app.js
@@ -11,13 +11,24 @@ function sortear(){
         while(numeroSorteados.includes(numero));{
             numero = sorteadorDeNumero(numeroMinimo, numeroMaximo);
         }
-       
-        numeroSorteados.push(numero);
+       numeroSorteados.push(numero);
     } 
     let resultado = document.getElementById('resultado');
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${numeroSorteados}</label>`;
+    alterarStatusBtn ();
 }
 
 function sorteadorDeNumero(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBtn(){
+    const btnReiniciar = document.getElementById('btn-reiniciar');
+    if (btnReiniciar.classList.contains('container__botao-desabilitado')){
+        btnReiniciar.classList.remove('container__botao-desabilitado');
+        btnReiniciar.classList.add('container__botao')
+    }else{
+        btnReiniciar.classList.remove('container__botao');
+        btnReiniciar.classList.add('container__botao-desabilitado')
+    }
 }
