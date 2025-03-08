7513d5798bc6128aa6a0ad60b4e44c459d0aa1f2
Rossoni <lucas.rossonidd@gmail.com>
2025-03-08 19:31:01 -0300

    Variável numero colocada dentro do loop, evitando o bug que estava rolando

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ function sortear() {
     let numeroMaximo = Number(document.getElementById('ate').value);
 
     let numeroSorteados = [];
-    let numero
 
     for (let i = 0; i < quantidadeDeNumeros; i++ ) {
-        numero = sorteadorDeNumero(numeroMinimo, numeroMaximo);
+        let numero = sorteadorDeNumero(numeroMinimo, numeroMaximo);
         
         while(numeroSorteados.includes(numero)){
             numero = sorteadorDeNumero(numeroMinimo, numeroMaximo);
@@ -38,6 +37,6 @@ function reiniciar(){
     document.getElementById('quantidade').value = '';
     document.getElementById('de').value = '';
     document.getElementById('ate').value = '';
-    document.getElementById('resultado').innerHTML = <label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>;
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
     alterarStatusBtn();
 }
